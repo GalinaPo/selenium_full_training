ed330dc38d62db5611d5a2b9708b95bc3592a596
GalinaPo <shine19@bk.ru>
2017-11-27 10:32:16 +0300

    задание14

--- java-example/src/test/java/ru/stqa/training/selenium/OpeningLinksNewWindow.java
+++ java-example/src/test/java/ru/stqa/training/selenium/OpeningLinksNewWindow.java
@@ -3,9 +3,14 @@ package ru.stqa.training.selenium;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 
+import javax.annotation.Nullable;
 import java.util.List;
+import java.util.Set;
 
 public class OpeningLinksNewWindow extends TestBase  {
 
@@ -25,10 +30,25 @@ public class OpeningLinksNewWindow extends TestBase  {
   public void openingLinksNewWindow() {
     wd.findElement(By.linkText("Countries")).click();
     wd.findElement(By.cssSelector("a[title='Edit']")).click();
-    List<WebElement> elements = wd.findElements(By.cssSelector("a[target='_blank']"));
-    for (int i = 0; i < elements.size(); i++) {
-      List<WebElement> el = wd.findElements(By.cssSelector("a[target='_blank']"));
-      el.get(i).findElement(By.cssSelector("a[target='_blank']")).click();
+    List<WebElement> linksList = wd.findElements(By.cssSelector("a[target='_blank']"));
+    for (WebElement link : linksList) {
+      String mainWindow = wd.getWindowHandle();
+      Set<String> oldWindows = wd.getWindowHandles();
+      link.click();
+      String newWindow = wait.until(anyWindowOtherThan(oldWindows));
+      wd.switchTo().window(newWindow);
+      wd.close();
+      wd.switchTo().window(mainWindow);
     }
+  }
+
+  public ExpectedCondition<String> anyWindowOtherThan(Set<String> oldWindows) {
+    return new ExpectedCondition<String>() {
+      public String apply(WebDriver wd) {
+       Set<String> handles = wd.getWindowHandles();
+       handles.removeAll(oldWindows);
+       return handles.size() > 0 ? handles.iterator().next() : null;
+      }
+    };
   }
 }
